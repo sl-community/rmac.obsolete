285a47b459101e9556f067f373192921192801bf
Axel Miesen <miesen@quadraginta-duo.de>
2019-03-03 15:59:28 +0100

    Minor fix

--- .cicd/sql-ledger/build/web/ledgersetup.pl
+++ .cicd/sql-ledger/build/web/ledgersetup.pl
@@ -26,7 +26,10 @@ my $instances = YAML::Tiny->read( '/ledgersetup.yml' )->[0]{instances};
 
 
 
-my ($instance) = grep { $_->{name} eq $instance_identifier || exists $_->{id} && $_->{id} eq $instance_identifier} @$instances;
+my ($instance) = grep {
+    (exists $_->{name} && $_->{name} eq $instance_identifier)
+        || (exists $_->{id} && $_->{id} eq $instance_identifier)
+    } @$instances;
 
 defined $instance
     || die "Instance with identifier '$instance_identifier' not found in config\n";
