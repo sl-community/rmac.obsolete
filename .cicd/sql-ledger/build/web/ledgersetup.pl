7e6e452bf561d6f3f7c40998721245bcf6364a4f
Axel Miesen <miesen@quadraginta-duo.de>
2019-03-08 14:26:53 +0100

    Changed ledgersetup.pl info output

--- .cicd/sql-ledger/build/web/ledgersetup.pl
+++ .cicd/sql-ledger/build/web/ledgersetup.pl
@@ -168,8 +168,7 @@ foreach my $user (@{$instance->{users}}) {
 
 status_and_exit(
     "Complete (" . scalar(@expanded_list) . " database(s): " .
-        join(", ", @expanded_list[0,1]) .
-        " [, ...])"
+        $expanded_list[0] . " [...])"
     );
 
 #############################################################################
